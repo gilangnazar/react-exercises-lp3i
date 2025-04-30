1025afbb22f93b39e9bcd0866298223e2846316c
gilangnazar <gilangnazar55@gmail.com>
2025-04-30 16:11:20 +0700

    formatif finished

--- formatif/frontend/src/components/Formdata.jsx
+++ formatif/frontend/src/components/Formdata.jsx
@@ -51,7 +51,7 @@ export default function Formdata(props){
       if(type === 'radio'){
         setForm({...form, [name]:value})
       } else if(type === "checkbox") {
-        setForm({...form, [name]: checked ? 'menikah' : 'belum menikah'})
+        setForm({...form, [name]: checked ? 'Menikah' : 'Belum Menikah'})
       } else {
         setForm({...form, [name]:value})
       }
@@ -61,6 +61,20 @@ export default function Formdata(props){
     async function handleFormSubmit(e) {
       e.preventDefault()
       await submitMahasiswa()
+    }
+
+    function handleClear(e) {
+      e.preventDefault()
+      setForm({
+        NIM: '',
+        Nama: '',
+        TanggalLahir: '',
+        StatusPernikahan: '',
+        Agama: '',
+        Alamat: '',
+        KodeJurusan: '',
+        BiayaKuliah: ''
+      })
     }
   
     useEffect(() => {
@@ -73,20 +87,20 @@ export default function Formdata(props){
             <form className="form" onSubmit={handleFormSubmit}>
               <div>
                 <label className="form-label">NIM</label>
-                <input type="text" name="NIM" className="form-control" onChange={handleInputChange}/>
+                <input type="text" name="NIM" value={form.NIM} className="form-control" onChange={handleInputChange}/>
               </div>
               <div>
                 <label className="form-label">Nama</label>
-                <input type="text" name="Nama" className="form-control" onChange={handleInputChange}/>
+                <input type="text" name="Nama" value={form.Nama} className="form-control" onChange={handleInputChange}/>
               </div>
               <div>
                 <label className="form-label">Tanggal Lahir</label>
-                <input type="date" name="TanggalLahir" className="form-control" onChange={handleInputChange}/>
+                <input type="date" name="TanggalLahir" value={form.TanggalLahir} className="form-control" onChange={handleInputChange}/>
               </div>
               <div>
                 <label className="form-label">Status Pernikahan</label>
                 <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="Menikah" onChange={handleInputChange} name="StatusPernikahan"/>
+                  <input className="form-check-input" type="checkbox" value="Tidak" checked={form.StatusPernikahan === 'Menikah' } onChange={handleInputChange} name="StatusPernikahan"/>
                   <label className="form-check-label" htmlFor="StatusPernikahan">
                     Menikah
                   </label>
@@ -95,31 +109,31 @@ export default function Formdata(props){
               <div>
                 <label className="htmlForm-label">Agama</label>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" value="Islam" onClick={handleInputChange} name="Agama" />
+                  <input className="form-check-input" type="radio" value="Islam" checked={form.Agama === 'Islam'} onChange={handleInputChange} name="Agama" />
                   <label className="form-check-label">
                     Islam
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" value="Protestan" onClick={handleInputChange} name="Agama" />
+                  <input className="form-check-input" type="radio" value="Protestan" checked={form.Agama === 'Protestan'} onChange={handleInputChange} name="Agama" />
                   <label className="form-check-label">
                     Protestan
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" value="Hindu" onClick={handleInputChange} name="Agama" />
+                  <input className="form-check-input" type="radio" value="Hindu" checked={form.Agama === 'Hindu'} onChange={handleInputChange} name="Agama" />
                   <label className="form-check-label">
                     Hindu
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" value="Budha" onClick={handleInputChange} name="Agama" />
+                  <input className="form-check-input" type="radio" value="Budha" checked={form.Agama === 'Budha'} onChange={handleInputChange} name="Agama" />
                   <label className="form-check-label">
                     Budha
                   </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" value="Katolik" onClick={handleInputChange} name="Agama" />
+                  <input className="form-check-input" type="radio" value="Katolik" checked={form.Agama === 'Katolik'} onChange={handleInputChange} name="Agama" />
                   <label className="form-check-label">
                     Katolik
                   </label>
@@ -127,11 +141,11 @@ export default function Formdata(props){
               </div>
               <div>
                 <label className="form-label">Alamat</label>
-                <input type="text" name="Alamat" className="form-control" onChange={handleInputChange}/>
+                <input type="text" name="Alamat" value={form.Alamat} className="form-control" onChange={handleInputChange}/>
               </div>
               <div>
                 <label className="form-label">Jurusan</label>
-                <select name="KodeJurusan" className="form-control" onChange={handleInputChange}>
+                <select name="KodeJurusan" value={form.KodeJurusan} className="form-control" onChange={handleInputChange}>
                   <option value="">--Pilih Jurusan--</option>
                   {jurusan.map((item, key) => (
                     <option value={item.KodeJurusan} key={key}>{item.Jurusan}</option>
@@ -140,10 +154,10 @@ export default function Formdata(props){
               </div>
               <div>
                 <label className="form-label">Biaya Kuliah</label>
-                <input type="text" name="BiayaKuliah" className="form-control" onChange={handleInputChange}/>
+                <input type="text" name="BiayaKuliah" value={form.BiayaKuliah} className="form-control" onChange={handleInputChange}/>
               </div>
               <button type="submit" className="m-1 btn btn-primary">Submit</button>
-              <button type="submit" className="m-1 btn btn-warning">Clear</button>
+              <button type="button" onClick={handleClear} className="m-1 btn btn-warning">Clear</button>
             </form>
         </>
     )
